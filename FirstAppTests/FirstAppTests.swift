49446be7729f05583edbb1f6d126b3ac73ebb690
ahmed gado <ahmedgado2001@gmail.com>
2025-04-15 23:04:27 +0200

    Initial Commit

---
+++ FirstAppTests/FirstAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppTests.swift
+//  FirstAppTests
+//
+//  Created by Ahmed Gado on 15/04/2025.
+//
+
+import Testing
+@testable import FirstApp
+
+struct FirstAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
